a49958d43ab3f52572be01132bdf1b8f3daf2b7b
Arwan Ahmad Khoiruddin <arwan.khoiruddin@gmail.com>
2019-06-27 22:06:17 +0800

    Updated collision algorithm using getBoundary

--- src/libs/Sprite.java
+++ src/libs/Sprite.java
@@ -1,5 +1,6 @@
 package libs;
 
+import javafx.geometry.Rectangle2D;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -39,29 +40,13 @@ public class Sprite extends ImageView {
         audioClip.play();
     }
 
-    public boolean collide(Sprite sprite) {
-        boolean isCollide = false;
-
-        if (this.intersects(sprite.getBoundsInParent()))
-            return true;
-
-        int yBottom = this.ypos + (int) Math.round(this.getImage().getHeight());
-        int xRight = this.xpos + (int) Math.round(this.getImage().getWidth());
-
-        int spriteBottom = sprite.ypos + (int) Math.round(sprite.getImage().getHeight());
-        int spriteRight = sprite.xpos + (int) Math.round(sprite.getImage().getWidth());
-
-        if (yBottom > sprite.ypos && ypos < sprite.ypos) {
-            if (xRight > sprite.xpos && xpos < sprite.xpos)
-                isCollide = true;
-        }
-
-        if (spriteBottom > this.ypos && sprite.ypos < this.ypos) {
-            if (spriteRight > this.xpos && sprite.xpos < this.xpos)
-                isCollide = true;
-        }
-
-        return isCollide;
+    public Rectangle2D getBoundary() {
+        return new Rectangle2D(this.xpos, this.ypos, this.getImage().getWidth(), this.getImage().getHeight());
     }
+
+    public boolean collide(Sprite sprite) {
+        return (this.getBoundary().intersects(sprite.getBoundary()));
+    }
+
 
 }
